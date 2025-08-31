db9b6cb8fac2c64fc33716270058e7bda4372b6e
mitanshrs <85603112+mitanshrs@users.noreply.github.com>
2025-08-31 22:38:06 +0530

    Add files via upload

---
+++ all/hitachi/L1.js
@@ -0,0 +1,74 @@
+export const DATA = {
+    TITLE_in_body: "Hitachi (L1)",
+    OPTIONS: {
+        "1": {
+            title: "Color Option",
+            unit: null,
+            list: {
+                "1": "White",
+                "2": "Pearl White"
+            }
+        },
+        "2": {
+            title: "Ton",
+            unit: " Ton",
+            list: {
+                "1": "1.0",
+                "2": "1.5",
+                "3": "2.0"
+            }
+
+        },
+        "3": {
+            title: "Stars",
+            unit: " Stars",
+            list: {
+                "1": "3",
+                "2": "5"
+            }
+        },
+        "4": {
+            title: "Year",
+            unit: " Year",
+            list: {
+                "1": "2023",
+                "2": "2024",
+                "3": "2025",
+            }
+        }
+    },
+
+    PTS: {
+        "1": {
+            SR: "RAS.B312PCAIBA1",
+            OPTION_SELECT: {
+                "1": "1",
+                "2": "1",
+                "3": "1",
+                "4": "3"
+            }
+        },
+        "2": {
+            SR: "RAS.G318PCCISS",
+            OPTION_SELECT: {
+                "1": "1",
+                "2": "2",
+                "3": "1",
+                "4": "3"
+            }
+        },
+        "3": {
+            SR: "RAS.Y518PCAISL2",
+            OPTION_SELECT: {
+                "1": "1",
+                "2": "2",
+                "3": "2",
+                "4": "3"
+
+            }
+        }
+    },
+    ERROR_MESSAGE_FT_NOT_MATCH: "No Hitachi product (L1) Matches Your Filter Options",
+    DES: "All Hitachi Ac (L1) all products which has one",
+    title_in_head: "Lakhi Airconditioners | Hitachi Ac (L1) (WORK)"
+}
---
+++ all/samsung/fr.js
@@ -0,0 +1,84 @@
+export const DATA = {
+    TITLE_in_body: "Samsung Refrigerator",
+    OPTIONS: {
+        "1": {
+            title: "Color Option",
+            unit: "",
+            list: {
+                "1": "BLACK CAVIR MATT / PET",
+                "2": "Black DOI",
+                "3": "SpaceMax™",
+                "4": "clean black",
+                "5": "Elegant Inox",
+                "6": "Ez Clean Steel",
+                "7": "Luxe Black",
+                "8": "Refinded Inox"
+            }
+        },
+        "2": {
+            title: "Star Rating",
+            unit: " Stars",
+            list: {
+                "1": "2",
+                "2": "3"
+            }
+        },
+        "3": {
+            title: "Capacity",
+            unit: "-L",
+            list: {
+                "1": "301",
+                "2": "322",
+                "3": "330",
+                "4": "350",
+                "5": "363",
+                "6": "385",
+                "7": "396",
+                "8": "415",
+                "9": "419",
+                "10": "465"
+            }
+        },
+        "4": {
+            title: "BEE year",
+            unit: " Year",
+            list: {
+                "1": "2023",
+                "2": "2024",
+                "3": "2025"
+            }
+        }
+    },
+    PTS: {
+        "1": {
+            SR: "RT34DG5A4DBXHL",
+            OPTION_SELECT: {
+                "1": "8",
+                "2": "2",
+                "3": "3",
+                "4": "3"
+            }
+        },
+        "2": {
+            SR: "RT34DG5A4DS8HL",
+            OPTION_SELECT: {
+                "1": "5",
+                "2": "2",
+                "3": "3",
+                "4": "3"
+            }
+        },
+        "3": {
+            SR: "RT51CG662BB1TL",
+            OPTION_SELECT: {
+                "1": "1",
+                "2": "1",
+                "3": "10",
+                "4": "2"
+            }
+        }
+    },
+    ERROR_MESSAGE_FT_NOT_MATCH: "No Product Matches Your Filter Options",
+    DES: "All Samsung Bespoke AI Refrigerator",
+    title_in_head: "Lakhi Airconditioners | all samsung Bespoke ai Refrigerator"
+};

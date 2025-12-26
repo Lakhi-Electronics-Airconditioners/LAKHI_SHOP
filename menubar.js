ac9a39c615f702a43a81b3bb430b67d0c2524627
lakhi-airconditioners <lakhi.airconditioners.database@gmail.com>
2025-12-26 16:13:08 +0530

    Update menubar.js

--- menubar.js
+++ menubar.js
@@ -1,23 +1,12 @@
 var Menuitems = document.getElementById('Menuitems');
-
-// Set initial state
-if (Menuitems) {
-    Menuitems.style.maxHeight ="0px";
-}
-
+Menuitems.style.maxHeight ="0px";
 function menutoggle(){
-    if (Menuitems) {
-        if(Menuitems.style.maxHeight == "0px") { 
-            // Menu is opening
-            Menuitems.style.maxHeight= "200px";
-            
-            // Check for the dynamic function and run it when opening
-            if(typeof window.onMenuOpenCallback === 'function') {
-                window.onMenuOpenCallback();
-            }
-        } else {
-            // Menu is closing
-            Menuitems.style.maxHeight ="0px";
+    if(Menuitems.style.maxHeight == "0px")
+       { 
+         Menuitems.style.maxHeight= "200px"
         }
-    }
+    else
+        {
+        Menuitems.style.maxHeight ="0px";
+        }
 }

0622f1b00ee4e885ce154e76c1fdd07f1e588fd1
lakhi-airconditioners <lakhi.airconditioners.database@gmail.com>
2026-03-01 18:31:51 +0530

    hidden,plu uses added + index overall ac updated + top TV/AC rating system added

--- service-worker.js
+++ service-worker.js
@@ -12,6 +12,9 @@ const FILES_TO_CACHE = [
   'https://shop.lakhiairconditioners.com/img/NABR/apk-down.jpeg',
   'https://shop.lakhiairconditioners.com/img/log-w.png',
   'https://shop.lakhiairconditioners.com/menubar.js',
+  'https://shop.lakhiairconditioners.com/js/rw/footer.js',
+  'https://shop.lakhiairconditioners.com/js/rw/header.js',
+  'https://shop.lakhiairconditioners.com/js/rw/tacpp.js',
   'https://shop.lakhiairconditioners.com/DOWNLOADED CSS/fontawesome-free-6.7.2-web/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
   '//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js'

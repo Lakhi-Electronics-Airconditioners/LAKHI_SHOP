b7e68a51c4cf866b95a5676d227d9f1a9ec6dcaa
mitanshrs <85603112+mitanshrs@users.noreply.github.com>
2026-03-08 22:06:46 +0530

    Update footer.js

--- js/rw/footer.js
+++ js/rw/footer.js
@@ -48,7 +48,7 @@ const footerContent = `
                     <div class="trustpilot-widget" data-locale="en-GB" data-template-id="56278e9abfbbba0bdcd568bc" data-businessunit-id="66d2011df3f7fe4eab3948e1" data-style-height="52px" data-style-width="50%" data-token="90f6214c-ff58-439e-8e8e-cd4fccd0223b">
                         <button class="greview" onclick="window.open('https://uk.trustpilot.com/review/lakhiairconditioners.com', '_blank')" style="background: white; display: inline; color: rgb(35, 35, 35); border: 1px solid #18f268; padding: 10px 20px; border-radius: 5px; cursor: pointer; font-size: 16px;">
                             review us on <br>
-                            <span style="font-weight: bold;"><i class="fab fa-trustpilot"></i> Trustpilot</span>
+                            <span style="font-weight: bold;"><i class="fa-solid fa-star" style="color: rgb(0, 255, 183);"></i> Trustpilot</span>
                         </button>
                     </div>
                 </div>
